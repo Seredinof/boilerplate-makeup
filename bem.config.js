159f898a2207e66abd172b22d602a4826aa2d650
Серединов Алексей <as@leads.su>
2017-10-17 18:23:32 +0300

    fonts

--- bem.config.js
+++ bem.config.js
@@ -1,7 +1,7 @@
 let obj = {
     bundlesName: 'default.bundles',
     levels: ['common.blocks', 'default.blocks'],
-    mustDeps: ['bootstrap']
+    mustDeps: ['bootstrap', 'font-awesome']
 }
 
 module.exports = obj;
